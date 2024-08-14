ea14c0435fe92ab703d7d17dabb0c0fc28f99bb5
unknown <kaflevola@gmail.com>
2024-08-14 19:39:58 +0545

    first class function updated

--- first-class-function.js
+++ first-class-function.js
@@ -24,3 +24,25 @@ var display = () => {
 }
 display()
 
+// passing a function as an argument
+// We are passing our sayHello() function as an argument to the message() function, this explains how we are treating the function as a value
+// The function that we pass as an argument to another function is called a callback function. sayHello() is a callback function
+function sayHello(){
+    return "Hello"
+}
+
+function message(sayHello,name){
+    console.log(sayHello()+" "+name+"!")
+}
+
+message(sayHello,"Bhola")
+
+// returning a function
+//In this example, we are returning a function from another function - We can return a function because functions in JavaScript are treated as values.
+function hello(){
+    return () => {
+        console.log("Function returning a function")
+    }
+}
+hello()()
+
